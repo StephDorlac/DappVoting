4fc3fdf9aaec946a52b1c6577d49f02e3e64e63c
Stephane Dorlac <sdotechnologies@gmail.com>
2021-12-06 14:43:39 +0100

    Updates

---
+++ precache-manifest.9ad07a994b426a6e87596bd2ba602b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c631fdc74f6accbb09def038e04408b",
+    "url": "/AlyraVoting/index.html"
+  },
+  {
+    "revision": "3425afc5b88dabda3fd8",
+    "url": "/AlyraVoting/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "45b1645e431c7d90939f",
+    "url": "/AlyraVoting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3425afc5b88dabda3fd8",
+    "url": "/AlyraVoting/static/js/2.1b86dd9b.chunk.js"
+  },
+  {
+    "revision": "45b1645e431c7d90939f",
+    "url": "/AlyraVoting/static/js/main.3cc03375.chunk.js"
+  },
+  {
+    "revision": "adf9affd7a554b7acb8d",
+    "url": "/AlyraVoting/static/js/runtime-main.c631bc18.js"
+  }
+]);
